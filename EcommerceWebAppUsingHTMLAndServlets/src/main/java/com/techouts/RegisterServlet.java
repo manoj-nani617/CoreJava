2ed4c00fda66bc51a01544c0b0ed8036f9ce9b5b
manoj-nani617 <manoj.k@techouts.co.in>
2026-01-28 08:29:30 +0530

    Edited on 28-11-2026

--- EcommerceWebAppUsingHTMLAndServlets/src/main/java/com/techouts/RegisterServlet.java
+++ EcommerceWebAppUsingHTMLAndServlets/src/main/java/com/techouts/RegisterServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -18,18 +19,40 @@ public class RegisterServlet extends HttpServlet{
 		String email = (String)req.getParameter("email");
 		String password = (String)req.getParameter("password");
 		String confirmPassword = (String)req.getParameter("confirmPassword");
-		try(Connection con = DriverManager.getConnection("jdbc:postgresql://localhost5432/Employee","postgres","manojkasu")) {
-			
-			PreparedStatement st = con.prepareStatement("insert into empdetails values(?,??)");
-			st.setString(1, username);
-			st.setString(2, email);
-			st.setString(3, password);
+		res.getWriter().println(username);
+		res.getWriter().println(email);
+		res.getWriter().println(password);
+		res.getWriter().println(confirmPassword);
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Employee","postgres","manojkasu");
+			res.getWriter().println("Connection Successful");
+			PreparedStatement st = con.prepareStatement("select count(*) from empdetails");
+			int count = 0;
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				count = rs.getInt(1);
+			}
+			if(count > 0) res.getWriter().println("No. of Users Exist "+count);
+			else res.getWriter().println("No. Of Users Exist "+count);
+			count++;
+			PreparedStatement st1 = con.prepareStatement("insert into empdetails values(?,?,?)");
+			st1.setInt(1, count);
+			st1.setString(1, username);
+			st1.setString(2, email);
+			st1.setString(3, password);
+			int rowsAffected = st1.executeUpdate();
+			res.getWriter().println("Rows Affected "+rowsAffected);
 			
 			
 			
 		}
 		catch(SQLException e) {
 			res.getWriter().println(e.getMessage());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		
